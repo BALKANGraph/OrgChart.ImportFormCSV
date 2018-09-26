17a8926a82de807e1af4c233a99b7ce23b51f669
BALKANGraph <plamen.peshev@live.com>
2018-09-26 17:18:13 +0300

    Initial commit

---
+++ OrgChart.ImportFormCSV/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrgChart.ImportFormCSV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Read()
+        {
+            var path = Server.MapPath("/App_Data/OrgChart.csv");
+
+            var csv = new List<string[]>();
+            var lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+                csv.Add(line.Split(','));
+
+            string json = new
+                System.Web.Script.Serialization.JavaScriptSerializer().Serialize(csv);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Write()
+        {
+            var path = Server.MapPath("/App_Data/OrgChart.csv");
+
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                var file = Request.Files[i];
+                var fileName = Path.GetFileName(file.FileName);
+                file.SaveAs(path);
+            }
+
+            var csv = new List<string[]>();
+            var lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+                csv.Add(line.Split(','));
+
+            string json = new
+                System.Web.Script.Serialization.JavaScriptSerializer().Serialize(csv);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
